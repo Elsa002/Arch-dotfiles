9dd941230c5dc2d305db42aa3ad342872e7390d0
Elsa002 <elsa002.linux@gmail.com>
2022-03-05 20:04:18 +0200

    Update polybar

--- source/home/.config/polybar/launch.sh
+++ source/home/.config/polybar/launch.sh
@@ -2,6 +2,13 @@
 
 DIR="$HOME/.config/polybar"
 
+# top bars: top, top-minimal, small-top
+# bottom bars: bottom, bottom-minimal, small-bottom
+# MAIN_TOP_BAR="top"
+# MAIN_BOTTOM_BAR="bottom"
+# VERTICAL_TOP_BAR="small-top"
+# VERTICALMAIN_BOTTOM_BAR="small-bottom"
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -9,19 +16,19 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.001; done
 
 # read screen orientation
-read -r line < <($DIR/read_orientation.sh) 
+read -r line < <($DIR/read_orientation.sh)
 
 # Launch bars
 if [[ "$line" == "normal" || "$line" == "bottom-up" ]]; then
-	polybar top &
-	polybar bottom &
+	polybar $MAIN_TOP_BAR &
+	polybar $MAIN_BOTTOM_BAR &
 	echo "Bars launched(top, bottom)..."
 elif [[ "$line" == "right-up" || "$line" == "left-up" ]]; then
-	polybar small-top &
-	polybar small-bottom &
+	polybar $VERTICAL_TOP_BAR &
+	polybar $VERTICALMAIN_BOTTOM_BAR &
 	echo "Bars launched(small-top, small-bottom)..."
 else
-	polybar top &
-	polybar bottom &
+	polybar $MAIN_TOP_BAR &
+	polybar $MAIN_BOTTOM_BAR &
 	echo "Bars launched(top, bottom)..."
 fi
